43226a605932df77e92814587c7ea3b612829c8e
mark0cv10 <cvjetkovicm058@gmail.com>
2025-09-15 21:51:40 +0200

    completed cleanup for 17.0

--- prod_desc_ai/__manifest__.py
+++ prod_desc_ai/__manifest__.py
@@ -4,32 +4,49 @@
     'license': 'LGPL-3',
     'category': 'Sales/Products',
     'summary': 'Automatically generate product descriptions using AI based on product images.',
-    'description': """
-Product AutoDescriber AI leverages the power of OpenAI's ChatGPT to automatically generate
-unique and engaging descriptions for your products based on their images.
-
-**Key Features:**
-- Automatically generates product descriptions using AI.
-- Supports multiple tones and languages to tailor the content to your brand's voice.
-- Integrates seamlessly with your existing product templates in Odoo.
-
-**Requirements:**
-To use this module, an **OpenAI API key** is required.
-
-**Configuration:**
-Go to Settings → Parameters → System Parameters and add:
-- Key: gpt_opd_api_key
-- Value: <Your OpenAI API Key>
-""",
+    "description": """
+        Product AutoDescriber AI leverages the power of OpenAI's ChatGPT to automatically generate unique and engaging descriptions for your products based on their images. This module is perfect for e-commerce businesses looking to enhance their product listings with high-quality AI-generated content.
+        
+        **Key Features:**
+        - Automatically generates product descriptions using AI.
+        - Supports multiple tones and languages to tailor the content to your brand's voice.
+        - Integrates seamlessly with your existing product templates in Odoo.
+        
+        **Requirements:**
+        To use this module, an **OpenAI API key** is required. This key allows the module to interact with OpenAI's services to generate descriptions based on the images of your products.
+        
+        **Getting an OpenAI API Key:**
+        1. Visit OpenAI’s official website and sign up for an account if you do not have one.
+        2. Navigate to the API section and apply for access to the API. Depending on your usage, you may need to provide details about your use case.
+        3. Once approved, you will receive an API key. This key is essential for the module to function and should be entered in the Odoo configuration panel under the settings specified by this module.
+        
+        **Configuring the API Key in Odoo:**
+        After obtaining the API key, go to Odoo’s Settings → Parameters → System Parameters and add a new parameter:
+        - Key: `gpt_opd_api_key`
+        - Value: `<Your OpenAI API Key>`
+        
+        Ensure you replace `<Your OpenAI API Key>` with the actual key provided by OpenAI. This key will be securely stored and used to authenticate requests from your Odoo instance to OpenAI.
+        
+        **Security Note:**
+        Keep your API key confidential to prevent unauthorized usage, which could lead to unexpected charges or data breaches.
+        
+        With Product AutoDescriber AI, enrich your product presentations and engage your customers like never before. Enhance your online shop's appeal and functionality with cutting-edge AI technology.
+        
+        This module currently supports Odoo 17. For earlier versions, please contact us for compatibility information.
+    """,
     'sequence': 10,
     'author': 'Wolfinne IT',
     'maintainer': 'Bojan Nisevic',
     'website': 'https://wolfinne.com',
     'support': 'bojan@wolfinne.com',
     'depends': ['website_sale'],
-    'images': ['static/description/thumbnail.png'],
+    'images': [
+        'static/description/thumbnail.png',
+    ],
     'external_dependencies': {
-        'python': ['openai'],
+        'python': [
+            'openai',
+        ],
     },
     'data': [
         'views/product_views.xml',
--- prod_desc_ai/models/product_template.py
+++ prod_desc_ai/models/product_template.py
@@ -1,5 +1,6 @@
 import logging
 from openai import OpenAI, APIError
+
 from odoo import _, models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools import image_data_uri
@@ -14,7 +15,6 @@ class ProductTemplate(models.Model):
         _logger.info(f'Update product (ID: {product_id}) description by AI in {desc_tone} tone.')
 
         product = self.browse(product_id)
-
         if not product.image_1920:
             _logger.error(_('Product image is not available.'))
             if server_action:
@@ -28,34 +28,33 @@ class ProductTemplate(models.Model):
             raise UserError(_('API key is not set.'))
 
         model = self.env['ir.config_parameter'].get_param('gpt_opd_model', default='gpt-4o')
-        max_tokens = int(self.env['ir.config_parameter'].get_param('gpt_opd_max_tokens', default=300))
+        max_tokens = self.env['ir.config_parameter'].get_param('gpt_opd_max_tokens', default=300)
         image_url = image_data_uri(product.image_1920)
-
         content = [
             {
                 "type": "text",
-                "text": (
-                    f"Describe this product in {desc_tone} tone. "
-                    f"Name of the product is {product.name}. "
-                    f"The description should be in language with locale code {desc_lang} "
-                    f"and should be suitable for e-commerce. "
-                    f"Do not use lists and bullet points. "
-                    f"Do not style the text, except emoticons when those are suitable."
-                ),
+                "text": f"Describe this product in {desc_tone} tone. Name of the product is {product.name}. "
+                        f"The description should be in language with locale code {desc_lang} and should be "
+                        f"suitable for e-commerce. Do not use lists and bullet points. Do not style the text, "
+                        f"except emoticons when those are suitable and convenient for the use."
             },
             {
-                "type": "image_url",
-                "image_url": {"url": image_url},
+              "type": "image_url",
+              "image_url": {
+                "url": image_url,
+              },
             },
         ]
-
         client = OpenAI(api_key=api_key)
 
         try:
             response = client.chat.completions.create(
-                model=model,
-                messages=[{"role": "user", "content": content}],
-                max_tokens=max_tokens,
+                model = model,
+                messages = [{
+                    "role": "user",
+                    "content": content,
+                }],
+                max_tokens = max_tokens,
             )
         except APIError as e:
             _logger.error(f"OpenAI API Error: {str(e)}")
@@ -65,14 +64,15 @@ class ProductTemplate(models.Model):
             raise UserError(f"An unexpected error occurred: {str(e)}")
 
         product.write({'description_ecommerce': response.choices[0].message.content})
+
         return response.choices[0].message.content
 
     def button_update_description(self):
         desc_tone = self._context.get('desc_tone')
         if not desc_tone:
             raise UserError(_('Description tone is not set.'))
-
         lang = self.env.context.get('lang')
+
         for record in self:
             record.update_product_desc_ai(record.id, desc_tone, lang)
 
